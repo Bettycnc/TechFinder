ccdaa127067fa1924e23926e1955f7e4d34929ad
Bettycnc <betty.canac@hotmail.com>
2024-10-05 21:07:20 +0200

    files with chart

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,42 +1,45 @@
+
+<template>
+  <!-- search-->
+  <div class="banner"> <br>Découvrez les offres d’emplois les plus rechercher dans votre région.
+    <div class="flex items-center justify-center  mt-10" >
+    
+      <div class="mr-4">
+        <label class="block text-black text-base font-bold" for="Poste">Poste</label>
+        <input class="shadow appearance-none border rounded py-1 px-1 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="Poste" type="text" placeholder="Job title">
+      </div>
+
+      <div class="mr-4">
+        <label class="block text-balck text-base font-bold" for="Lieu">Lieu</label>
+        <input class="shadow appearance-none border rounded py-1 px-1 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="Lieu" type="text" placeholder="Où ?">
+      </div>
+
+      <button @click="handleSearch"
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-1 rounded-full mt-5">Recherche</button>
+    </div>
+  </div>
+  <p class="-ml-9 text-center text-lg"> Chiffres clés</p>
+  <ChartCompoment/>
+
+  
+
+</template>
 <script setup>
+import ChartCompoment from './ChartCompoment.vue'
 
 </script>
 
-<template>
-  <nav class="navbar"> 
-    <a href="#accueil">Accueil</a> 
-    <a href="#Estimation">Estimation de salaire</a>
-  </nav>
-  <header>
-    <title> Emplois les plus rechercher</title>
-  </header>
-
-  <main>
-    <div class="banner">
-    <h1> <br>Découvrez les offres d’emplois les plus rechercher dans votre région.</h1>
-      <div class="recherche">
-        <label for="Poste">Poste:</label>
-        <input type="text", Placeholder="Job Title"></input>
-      </div>
-    </div>
-  </main>
-</template>
 
 <style scoped>
 .banner {
-  background-color: rgba(45, 45, 190, 0.752);
+  background-color: rgb(130, 170, 161);
   width: 100%;
-  height: 200px;
+  height: 220px;
   text-align: center;
+  font-size: 2cap;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  
 }
-h1 {
-  font-size: 1.8cap;
-  font-style: normal;
-  font-weight: normal;
-  ;
-}
+
 .navbar {
   display: flex;
   justify-content: space-around;
@@ -48,13 +51,4 @@ h1 {
   text-decoration: none;
   color: #000000;
 }
-.recherche {
-  width: 40%;
-  background-color: white;
-  position: absolute;
-  left: 30%;
-  border: 2px solid;
-  border-color: black;
-}
-
 </style>
